149cf62a0bc7dc29954cf0ac16bafc78e61464e9
Elmira5555555 <elmirakerimovah@gmail.com>
2022-06-12 13:01:20 -0700

    added two task

---
+++ Writing some of the string's internal methods 2/Writing some of the string's internal methods 2/Program.cs
@@ -0,0 +1,32 @@
+﻿char[] Upper = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+char[] Lower = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+while (true)
+{
+    Console.WriteLine("please insert char : ");
+    string targetText = Console.ReadLine();
+    string result = AlphabetChanger(targetText, Upper, Lower);
+    Console.WriteLine($"Your changed name : {result}");
+
+    Console.WriteLine("please insert char : ");
+    string targetTextTwo = Console.ReadLine();
+    string resultTwo = AlphabetChanger2(targetTextTwo, Lower, Upper);
+    Console.WriteLine($"Your changed name : {resultTwo}");
+}
+static string AlphabetChanger(string Text, char[] upperAlphabet, char[] lowerAlphabet)
+{
+    for (int i = 0; i < upperAlphabet.Length; i++)
+    {
+        Text = Text.Replace(lowerAlphabet[i], upperAlphabet[i]);
+    }
+    return Text;
+}
+
+static string AlphabetChanger2(string Text, char[] lowerAlphabet, char[] upperAlphabet)
+{
+    for (int i = 0; i < lowerAlphabet.Length; i++)
+    {
+       Text = Text.Replace(upperAlphabet[i], lowerAlphabet[i]);
+    }
+    return Text;
+}
---
+++ Writing some of the string's internal methodss/Writing some of the string's internal methodss/Program.cs
@@ -0,0 +1,14 @@
+﻿static void Main(string[] args)
+{
+    char[] TargetUpperCaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    char[] TargetLowerCaseLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    while (true)
+    {
+        string[] commands = { "/Convert to uppercase letters", "/Convert to lowercase letters", "/determine the existence of a letter" };
+        Console.WriteLine("/Convert to uppercase letters");
+        Console.WriteLine("/Convert to lowercase letters");
+        Console.WriteLine("/determine the existence of a letter");
+        Console.WriteLine("please choose a command from above:");
+        string command = Console.ReadLine();
+    }
+}

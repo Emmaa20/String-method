472636936711dfa96fb802ef6c1223b66f384abc
Elmira5555555 <elmirakerimovah@gmail.com>
2022-06-12 08:55:21 -0700

    added new task

---
+++ Writing some of the string's internal methods/Writing some of the string's internal methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("press yes or no");
+        string targetForm = Console.ReadLine();
+        bool isCaseSensetive = false;
+
+        if (targetForm == "yes")
+        {
+            isCaseSensetive = true;
+        }
+        else if (targetForm == "no")
+        {
+            isCaseSensetive = false;
+        }
+        while (true)
+        {
+            Console.WriteLine("Please insert text:");
+            string targetName = Console.ReadLine();
+            Console.WriteLine("Please insert character:");
+            char targetChar = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("result:");
+            Console.WriteLine(IsLetterExists(targetName, targetChar));
+            Console.WriteLine(" ");
+        }
+        static bool IsLetterExists(string targetName, char targetChar)
+        {
+            for (int i = 0; i < targetName.Length; i++)
+            {
+                if (targetChar == targetName[i])
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+    }
+}
